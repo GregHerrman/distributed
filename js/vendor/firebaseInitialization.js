c59cec1f52963d8ab8c81d663a541e92430a8da0
GregHerrman <33264207+GregHerrman@users.noreply.github.com>
2017-10-31 13:18:07 -0500

    Update vendor config

--- js/vendor/firebaseInitialization.js
+++ js/vendor/firebaseInitialization.js
@@ -1,10 +1,10 @@
 var config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGE_ID"
+  apiKey: "AIzaSyApPJfuVrN2q5-NP1Tbr60WIx70fKhacRg",
+  authDomain: "alva-funretro.firebaseapp.com",
+  databaseURL: "alva-funretro.firebaseio.com",
+  projectId: "alva-funretro",
+  storageBucket: "alva-funretro.appspot.com",
+  messagingSenderId: "809769579815"
 };
 
 firebase.initializeApp(config);
